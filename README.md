c9d9d92c39c3ca73fe035ce34aabb2adb410cc6e
Adam Gerard <adam.intae.gerard@protonmail.com>
2024-04-21 13:01:00 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# EXP_WHITEBOARD_JS
+
+An experiment to get more acquainted with the [Canvas API](https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API) (I was looking into this a long time ago and wanted to revisit this topic).
+
+I was also concerned by the lack of simple drawing tools and found the following example inspiring: https://github.com/os1ma/handwriting-canvas/blob/main/LICENSE. Whole companies and websites have launched to do this kind of thing and I was surprised at how easy it is to implement from scratch.
+
+Good to practice proper Library and SDK design.
+
+## Use
+
+### Drawing
+
+1. Use your mouse to draw lines, curves, etc. 
+2. **Hold-Down** the **Left-Mouse-Button** to do so.
+
+### Erase
+
+1. Select **Erase**.
+1. Use your mouse to erase specific parts of your drawings.
+2. **Hold-Down** the **Left-Mouse-Button** to do so.
+
+### Clear the Whiteboard (Entirely)
+
+1. Select **Clear** to reset the page/return it to its blank state.
+
+### Save
+
+1. Select **Save** to download your drawing.
+2. (You can import these or use them to generate an inventory of Machine Learning items for [Numpy](https://numpy.org/) - say for Image Recognition).
+
---
+++ writingpanel.js
@@ -0,0 +1,125 @@
+"use strict"
+
+/**
+ * @Author - Adam In Tae Gerard - https://www.linkedin.com/in/adamintaegerard/
+ *
+ * Scratch-built handwriting lib for HTML5 Canvas.
+ * 
+ * Helpful examples for drawing circles and curved lines: https://github.com/os1ma/handwriting-canvas/blob/main/LICENSE
+ */
+
+const reset = that => {
+    that.context.strokeStyle = 'red'
+    that.context.lineCap = 'round'
+    that.context.lineJoin = 'round'
+    that.context.lineWidth = 5
+    that.isEraser = false
+}
+
+var WritingPanel = function (els) {
+    this.lastX = 0
+    this.lastY = 0
+    this.isDown = false
+    this.initialized = false
+    this.isEraser = false
+
+    if (els.length > 1) new Error("Element not uniquely specified!")
+
+    this.el = els[0]
+    this.context = this.el.getContext("2d")
+}
+
+WritingPanel.prototype.getMousePos = function (e) {
+    const R = this.el.getBoundingClientRect()
+    const M = {}
+    M["X"] = parseInt(e.clientX - R.left)
+    M["Y"] = parseInt(e.clientY - R.top)
+    return M
+}
+
+WritingPanel.prototype.drawLine = function (startX, startY, endX, endY) {
+    this.context.beginPath()
+    this.context.moveTo(startX, startY)
+    this.context.lineTo(endX, endY)
+    this.context.stroke()
+    this.context.closePath()
+}
+
+WritingPanel.prototype.toBlobRect = function() {
+    // For generating CNN images
+}
+
+WritingPanel.prototype.toPng = function() {
+    let img = this.context.getImageData(0,0,2500,2500)
+
+    for (let i = 0; i < img.data.length; i+= 4) {
+        let r = img.data[i]
+        let g = img.data[i+1]
+        let b = img.data[i+2]
+
+        if (r === 255 && g === 255 && b === 255) {
+            img.data[i] = 255
+            img.data[i+1] = 255
+            img.data[i+2] = 255
+            img.data[i+3] = 0
+        }
+    }
+
+    this.context.putImageData(img, 0, 0)
+    return this.el.toDataURL("image/png")
+}
+
+WritingPanel.prototype.erase = function() {
+
+    this.isEraser = !this.isEraser
+
+    if (this.isEraser) {
+        this.context.strokeStyle = 'white'
+        this.context.lineCap = 'round'
+        this.context.lineJoin = 'round'
+        this.context.lineWidth = 15
+    
+    } else reset(this)
+}
+
+WritingPanel.prototype.init = function () {
+    if (!this.initialized) {
+        reset(this)
+        this.initialized = !this.initialized
+
+        this.el.addEventListener('mousedown', e => {
+            this.isDown = true
+            const M = this.getMousePos(e)
+            this.lastX = M.X
+            this.lastY = M.Y
+            e.preventDefault
+        })
+
+        this.el.addEventListener('mouseup', e => {
+            if (this.isDown) {
+                const M = this.getMousePos(e)
+                this.drawLine(this.lastX, this.lastY, M.X, M.Y)
+                this.isDown = !this.isDown
+            }
+        })
+
+        this.el.addEventListener('mousemove', e => {
+            if (this.isDown) {
+                const M = this.getMousePos(e)
+                this.drawLine(this.lastX, this.lastY, M.X, M.Y)
+                this.lastX = M.X
+                this.lastY = M.Y
+            }
+        })
+    }
+}
+
+WritingPanel.prototype.clear = function() {
+    this.context.clearRect(
+        0, 
+        0, 
+        this.context.canvas.width, 
+        this.context.canvas.height
+    )
+    reset(this)
+}
