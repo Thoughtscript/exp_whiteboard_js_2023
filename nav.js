eb314c87294d18ed1d35fd987ff617cb273a9cab
Adam Gerard <adam.intae.gerard@protonmail.com>
2024-04-22 15:15:52 -0500

    tweak ui on modal

--- nav.js
+++ nav.js
@@ -18,8 +18,11 @@ var isMobile = function () {
     for (var i = 0; i < mobileMatches.length; i++) {
         // Old and newer iPads
         if (user.includes(mobileMatches[i]) || navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1) {
+            document.getElementById("reset").style.display = "none";
+            document.getElementById("save").style.display = "none";
+            document.getElementById("erase").style.display = "none";
             window.location.href = "https://render-static-fs.onrender.com/study_guide_notes_2023.html";
-            alert("This demo isn't optimized for mobile touch support! Please visit me on a desktop!")
+            alert("This demo isn't optimized for mobile touch support! Please visit me on a desktop!");
             break;
         }
     }
